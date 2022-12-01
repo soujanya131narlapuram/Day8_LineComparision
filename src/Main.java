05e1e13590070d2a989d4db8defd2661ec35fba2
soujanya131narlapuram <soujanya131narlapuram@gmail.com>
2022-12-02 00:12:02 +0530

    'master commit'

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line\n" +
+                "Comparison Computation\n" +
+                "Program");
+    }
+}
