634dff46b943ff2d488f79df98a2f3bcd19d6043
soujanya131narlapuram <soujanya131narlapuram@gmail.com>
2022-12-02 00:31:10 +0530

    'uc2 commit'

--- src/OopsLineComparision.java
+++ src/OopsLineComparision.java
@@ -4,7 +4,6 @@ public class OopsLineComparision {
     int y1;
     int x2;
     int y2;
-    double line1_length;
     public  OopsLineComparision(int x1,int y1,int x2,int y2)
     {
         this.x1=x1;
@@ -12,15 +11,30 @@ public class OopsLineComparision {
         this.x2=x2;
         this.y2=y2;
     }
-    public  void length_of_line1()
+    public  double length_of_line1(int x1,int y1,int x2, int y2)
     {
         double l1 = Math.pow((x2 - x1), 2);
         double l2 = Math.pow((y2 - y1), 2);
         double line1_length = Math.sqrt(l1 + l2);
         System.out.println("length of line one is:"+line1_length);
+        return  line1_length;
+    }
+    public void  equals_or_not(double line1_length,double line2_length)
+    {
+        if(line1_length==line2_length) {
+            System.out.println("both lines are equal");
+        }
+        else{
+            System.out.println("both lines are not equal");
+        }
     }
     public static void main(String[] args) {
         OopsLineComparision obj=new OopsLineComparision(4, 6, 6, 8);
-        obj.length_of_line1();
+        double line1_len=obj.length_of_line1(obj.x1,obj.y1,obj.x2,obj.y2);
+
+        OopsLineComparision obj2=new OopsLineComparision(4, 9, 6, 8);
+        double line2_len=obj2.length_of_line1(obj2.x1,obj2.y1,obj2.x2,obj2.y2);
+
+        obj.equals_or_not(line1_len,line2_len);
     }
 }
